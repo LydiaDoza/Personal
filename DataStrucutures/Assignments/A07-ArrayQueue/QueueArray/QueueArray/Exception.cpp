c792ad3a6b35547850132effcfab4f040c0d6101
Lydia Doza <Lydiasaurus@users.noreply.github.com>
2017-04-13 19:44:56 -0700

    Reduce whitespace

--- DataStrucutures/Assignments/A07-ArrayQueue/QueueArray/QueueArray/Exception.cpp
+++ DataStrucutures/Assignments/A07-ArrayQueue/QueueArray/QueueArray/Exception.cpp
@@ -1,23 +1,16 @@
 #include "Exception.h"
 
-
 /**********************************************************************
 * Purpose:	Default constructor
-*
 * Entry:	None
-*
 * Exit:		Exception instance created, m_msg set to nullptr
 ************************************************************************/
 Exception::Exception() : m_msg(nullptr)
-{
-
-}
+{}
 
 /**********************************************************************
 * Purpose:	1 argument ctor
-*
 * Entry:	char * msg to send to exception
-*
 * Exit:		m_msg is set to msg
 ************************************************************************/
 Exception::Exception(char * msg) : m_msg(msg)
@@ -28,9 +21,7 @@ Exception::Exception(char * msg) : m_msg(msg)
 
 /**********************************************************************
 * Purpose:	copy ctor
-*
 * Entry:	Exception copy
-*
 * Exit:		this is equal to copy
 ************************************************************************/
 Exception::Exception(const Exception & copy) : m_msg(nullptr)
@@ -41,9 +32,7 @@ Exception::Exception(const Exception & copy) : m_msg(nullptr)
 
 /**********************************************************************
 * Purpose:	Destructor
-*
 * Entry:	None.
-*
 * Exit:		deletes m_msg, sets it to nullptr
 ************************************************************************/
 Exception::~Exception()
@@ -54,9 +43,7 @@ Exception::~Exception()
 
 /**********************************************************************
 * Purpose:	Gets Message of thrown exception
-*
 * Entry:	None.
-*
 * Exit:		m_msg is returned
 ************************************************************************/
 const char* Exception::GetMessage()
@@ -66,9 +53,7 @@ const char* Exception::GetMessage()
 
 /**********************************************************************
 * Purpose:	Sets m_msg to input msg
-*
 * Entry:	char * msg
-*
 * Exit:		m_msg set to msg
 ************************************************************************/
 void Exception::SetMessage(char * msg)
@@ -78,9 +63,7 @@ void Exception::SetMessage(char * msg)
 
 /**********************************************************************
 * Purpose:	overloads = sign to handle Exceptions
-*
 * Entry:	Exception rhs
-*
 * Exit:		this is set to rhs
 ************************************************************************/
 Exception& Exception::operator=(const Exception & rhs)
@@ -97,9 +80,7 @@ Exception& Exception::operator=(const Exception & rhs)
 
 /**********************************************************************
 * Purpose:	overloads << to print the message of the Exception
-*
 * Entry:	ostream stream by ref, const ref Exception
-*
 * Exit:
 ************************************************************************/
 ostream& operator<<(ostream & stream, const Exception & except)
